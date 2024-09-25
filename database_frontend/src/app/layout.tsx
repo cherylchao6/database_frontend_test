bf799f3193a813f9c8c205067b73b0b9f6a7a198
cherylchao6 <nmpyt21@gmail.com>
2024-09-25 10:13:34 -0400

    feat: config docker

--- database_frontend/src/app/layout.tsx
+++ database_frontend/src/app/layout.tsx
@@ -1,12 +1,9 @@
 import "../styles/globals.css";
-import DashboardLayout from "@/components/DashboardLayout";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body>
-          {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 };
