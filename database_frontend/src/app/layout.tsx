ea50f67a81cc2a6d52a05e750e59609c9fe90192
Ruknuy <supachai5028@gmail.com>
2024-10-18 16:40:02 -0400

    fixed extra attributes

--- database_frontend/src/app/layout.tsx
+++ database_frontend/src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning>
         {/* Move the ThemeProvider inside the body */}
         <ThemeProvider attribute="class" defaultTheme="light">
