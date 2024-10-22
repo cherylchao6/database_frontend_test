0566da8844a5032a154898ab24b44b97faffda8b
Ruknuy <supachai5028@gmail.com>
2024-10-22 15:39:36 -0400

    fixed buttonprops

--- database_frontend/src/components/Button.tsx
+++ database_frontend/src/components/Button.tsx
@@ -4,6 +4,7 @@ interface ButtonProps {
   text: string;
   onClick?: () => void;
   disabled?: boolean;
+  className?: string;
 }
 
 const Button: React.FC<ButtonProps> = ({ text, onClick, disabled }) => {
