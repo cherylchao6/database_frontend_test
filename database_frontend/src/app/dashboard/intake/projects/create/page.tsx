6f66fcfefeaa114a4ffd36525b15e3ae8217dd45
cherylchao6 <nmpyt21@gmail.com>
2025-03-27 12:56:46 -0400

    feat: update project by call api

--- database_frontend/src/app/dashboard/intake/projects/create/page.tsx
+++ database_frontend/src/app/dashboard/intake/projects/create/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { Project } from "@/types/intakes/project";
 import ProjectForm from "@/components/ProjectForm";
-// import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { cleanObject } from "@/utils/apiHelper";
 import { useSession } from "next-auth/react";
 
@@ -40,18 +40,16 @@ const initialProjectData: Project = {
 };
 
 const CreateProjectPage = () => {
+  const router = useRouter();
   const { data: session } = useSession();
-  // console.log("session", session);
-  // const router = useRouter();
   const [error, setError] = useState<string | null>(null);
 
   const handleSave = async (newProjectData: Project) => {
     try {
       (newProjectData as any).locationId = newProjectData?.location?.id;
       (newProjectData as any).assignedTo = newProjectData?.assignedTo?.id;
-      const payload = cleanObject(newProjectData);
 
-      console.log("payload", payload);
+      const payload = cleanObject(newProjectData);
 
       const response = await fetch(`${apiUrl}/projects`, {
         method: "POST",
@@ -71,7 +69,7 @@ const CreateProjectPage = () => {
       }
 
       alert("Project created successfully!");
-      // // router.push("/dashboard/intake/projects");
+      router.push("/dashboard/intake/projects");
     } catch (err) {
       if (err instanceof Error) {
         setError(err.message);
